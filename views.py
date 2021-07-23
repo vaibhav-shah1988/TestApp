772b32f8f51fab2f1d3b4e39b7e80de0d4315d60
vaibhav-shah1988 <vaibhav_shah1988@yahoo.com>
2021-07-23 21:24:02 +0530

    Update views.py

--- views.py
+++ views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-from PythonWebProject import app
+from chartapp import app
 
 @app.route('/')
 @app.route('/home')
